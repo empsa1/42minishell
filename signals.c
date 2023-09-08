ab1e2d8861f3006ab1525edadf3d0ba2c0363a91
Eduardo manuel Portela da silva <eportela@c2r4s6.42lisboa.com>
2023-09-08 16:31:24 +0100

    changes

--- signals.c
+++ signals.c
@@ -1,1 +1,15 @@
-s
+#include "minishell.h"
+
+void sigint_handler(int signal)
+{
+    printf("wtf\n");
+    if (signal)
+        system("minishell");
+}
+
+int arm_signals()
+{
+    //changesgsgseges
+    signal(SIGINT, sigint_handler);
+    return (0);
+}
