871c664fa2d084fb7219432291be28e315255676
anda-cun <anda-cun@student.42lisboa.com>
2023-09-08 16:30:31 +0100

    added signals

--- signals.c
+++ signals.c
@@ -1,1 +1,14 @@
-s
+#include "minishell.h"
+
+void sigint_handler(int signal)
+{
+    printf("wtf\n");
+    if (signal)
+        system("./minishell");
+}
+
+int arm_signals()
+{
+    signal(SIGINT, sigint_handler);
+    return (0);
+}
